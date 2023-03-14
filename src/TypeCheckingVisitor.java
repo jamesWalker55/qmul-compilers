4459a7ef86aaa0f44b9a4c22ce6b285b26645408
James Walker <james.chunho@gmail.com>
2023-03-14 14:58:09 +0000

    working

--- src/TypeCheckingVisitor.java
+++ src/TypeCheckingVisitor.java
@@ -1,165 +1,217 @@
 import ast.*; //imports all the node types from the ast
 import ast.visitor.BaseVisitor;
 
+import ast.Symbol;
 
-class MyContext {
-    //this should have data about the context: O, M, C
-    //Objects O "A function to assign type T to object"
-    //Methods M 
-    //Class C
-    //currently unsure how context should be implemented/how it is used
-    //the info for this is in Type Environments in the manual
+import java.util.HashMap;
+import java.util.List;
+import java.util.Stack;
+import java.util.ArrayList;
+import java.util.Arrays;
 
-    //needs a list of identifiers via a symbol table (provided class)
-    //use a stack to push and pop when changing scope
-    SymbolTable<TableData> table = new SymbolTable<TableData>();
-    //Object[]
-    // where index 0 = variable or method or class
-    // where index 1 = type
-    // where index 2 = properties
+class MySymbolTable {
+    private Stack<HashMap<List<String>, TableData>> tbl = new Stack<HashMap<List<String>, TableData>>();
 
-    //constructor
-    public MyContext(){
-        table.enterScope(); //enters the first scope
+    public MySymbolTable() {
     }
 
-    //useful symbol table functions:
-    //addId(Symbol id, D info), enterScope(), exitScope()
-    //this function should be used when defining new functions/variables
+    /**
+     * Enters a new scope. A scope must be entered before anything
+     * can be added to the table.
+     */
+    public void enterScope() {
+        tbl.push(new HashMap<List<String>, TableData>());
+    }
 
-    public void addId(Symbol id, TableData info){ //unsure what type the data/info should be atm
-        table.addId(id, info);
+    /**
+     * Exits the most recently entered scope.
+     */
+    public void exitScope() {
+        if (tbl.empty()) {
+            Utilities.fatalError("existScope: can't remove scope from an empty symbol table.");
+        }
+        tbl.pop();
     }
-    public TableData lookup(Symbol sym){
-        return table.lookup(sym);
+
+    private List<String> createKey(Symbol id, String kind) {
+        return Arrays.asList(id.getName(), kind);
     }
-    public Symbol lookupVariable(Symbol sym){
-        return lookup(sym).type;
+
+    /**
+     * Adds a new entry to the symbol table.
+     *
+     * @param id   the symbol
+     * @param info the data associated with id
+     */
+    public void addId(Symbol id, String kind, TableData info) {
+        if (tbl.empty()) {
+            Utilities.fatalError("addId: can't add a symbol without a scope.");
+        }
+        tbl.peek().put(createKey(id, kind), info);
+    }
+
+    /**
+     * Looks up an item through all scopes of the symbol table. If
+     * found it returns the associated information field, if not it
+     * returns <code>null</code>.
+     *
+     * @param sym the symbol
+     * @return the info associated with sym, or null if not found
+     */
+    public TableData lookup(Symbol id, String kind) {
+        if (tbl.empty()) {
+            Utilities.fatalError("lookup: no scope in symbol table.");
+        }
+        // I break the abstraction here a bit by knowing that stack is
+        // really a vector.
+        for (int i = tbl.size() - 1; i >= 0; i--) {
+            TableData info = tbl.elementAt(i).get(createKey(id, kind));
+            if (info != null)
+                return info;
+        }
+        return null;
+    }
+
+    /**
+     * Probes the symbol table. Check the top scope (only) for the
+     * symbol <code>sym</code>. If found, return the information field.
+     * If not return <code>null</code>.
+     *
+     * @param sym the symbol
+     * @return the info associated with sym, or null if not found
+     */
+    public TableData probe(Symbol id, String kind) {
+        if (tbl.empty()) {
+            Utilities.fatalError("lookup: no scope in symbol table.");
+        }
+        return tbl.peek().get(createKey(id, kind));
+    }
+
+    /**
+     * Gets the string representation of the symbol table.
+     *
+     * @return the string rep
+     */
+    public String toString() {
+        String res = "";
+        // I break the abstraction here a bit by knowing that stack is
+        // really a vector...
+        for (int i = tbl.size() - 1, j = 0; i >= 0; i--, j++) {
+            res += "Scope " + j + ": " + tbl.elementAt(i) + "\n";
+        }
+        return res;
     }
 }
 
-class TableData{
-    String kind;
-    Symbol type;
+class TableData {
     Object properties;
 
-    public TableData(String kind, Symbol type, Object properties){
-        this.kind = kind;
-        this.type = type;
+    public TableData(Object properties) {
         this.properties = properties;
+    }
+
+    Object getProperties() {
+        return properties;
     }
 }
 
-public class TypeCheckingVisitor extends BaseVisitor<Symbol, MyContext> {
+public class TypeCheckingVisitor extends BaseVisitor<Symbol, MySymbolTable> {
 
-    //go down the abstract syntax tree
-    //then label each node with its type by proving the premises
+    // go down the abstract syntax tree
+    // then label each node with its type by proving the premises
 
     @Override
-    public Symbol visit(ProgramNode node, MyContext data){
-        //creates a new context when the program starts
-        //this context is passed down
-        
-        data = new MyContext();
+    public Symbol visit(ProgramNode node, MySymbolTable data) {
+        // creates a new context when the program starts
+        // this context is passed down
+
+        data = new MySymbolTable();
         return visit(node.getClasses(), data);
     }
 
     @Override
-    //for {∗, +, −, /} operations
-    public Symbol visit(IntBinopNode node, MyContext data){
+    // for {∗, +, −, /} operations
+    public Symbol visit(IntBinopNode node, MySymbolTable data) {
 
-        //if type is incorrect, send a semant error
-        //O, M, C |- e1 : Int
-        if (!visit(node.getE1(), data).equals(TreeConstants.Int))
-        {
-            //error format:
-            //filename:ln: non-Int arguments: E1.Type + E2.Type
+        // if type is incorrect, send a semant error
+        // O, M, C |- e1 : Int
+        if (!visit(node.getE1(), data).equals(TreeConstants.Int)) {
+            // error format:
+            // filename:ln: non-Int arguments: E1.Type + E2.Type
             Utilities.semantError().println("error here");
         }
-        //O, M, C |- e2 : Int
-        if (!visit(node.getE2(), data).equals(TreeConstants.Int))
-        {
-            Utilities.semantError().println("error here");;
+        // O, M, C |- e2 : Int
+        if (!visit(node.getE2(), data).equals(TreeConstants.Int)) {
+            Utilities.semantError().println("error here");
+            ;
         }
-        //op ∈ {∗, +, −, /}
-        //operation is allowed and creates an int
+        // op ∈ {∗, +, −, /}
+        // operation is allowed and creates an int
         node.setType(TreeConstants.Int);
         return visit((IntBinopNode) node, data);
     }
 
     @Override
-    public Symbol visit(ExpressionNode node, MyContext data){
-        //check the expression's type
-        //if it is a type set it
-        //else, visit node
-        //The rules shown in the manual:
-        if (node instanceof DispatchNode){
+    public Symbol visit(ExpressionNode node, MySymbolTable data) {
+        // check the expression's type
+        // if it is a type set it
+        // else, visit node
+        // The rules shown in the manual:
+        if (node instanceof DispatchNode) {
             visit((DispatchNode) node, data);
-        }
-        else if (node instanceof StaticDispatchNode){
+        } else if (node instanceof StaticDispatchNode) {
             visit((StaticDispatchNode) node, data);
-        }
-        else if (node instanceof CondNode){
+        } else if (node instanceof CondNode) {
             visit((CondNode) node, data);
-        }
-        else if (node instanceof LetNode){
+        } else if (node instanceof LetNode) {
             visit((LetNode) node, data);
-        }
-        else if (node instanceof CaseNode){
+        } else if (node instanceof CaseNode) {
             visit((CaseNode) node, data);
-        }
-        else if (node instanceof LoopNode){
+        } else if (node instanceof LoopNode) {
             visit((LoopNode) node, data);
-        }
-        else if (node instanceof IsVoidNode){
+        } else if (node instanceof IsVoidNode) {
             visit((IsVoidNode) node, data);
-        }
-        else if (node instanceof BoolUnopNode){
+        } else if (node instanceof BoolUnopNode) {
             visit((BoolUnopNode) node, data);
-        }
-        else if (node instanceof CompNode){
+        } else if (node instanceof CompNode) {
             visit((NegNode) node, data);
-        }
-        else if (node instanceof IntBinopNode){
+        } else if (node instanceof IntBinopNode) {
             visit((IntBinopNode) node, data);
-        }
-        else if (node instanceof EqNode){
+        } else if (node instanceof EqNode) {
             visit((EqNode) node, data);
-        }
-        else if (node instanceof ObjectNode){
+        } else if (node instanceof ObjectNode) {
             visit((ObjectNode) node, data);
         }
-        //basic types
-        else if (node instanceof IntConstNode){ 
+        // basic types
+        else if (node instanceof IntConstNode) {
             node.setType(TreeConstants.Int);
-        }
-        else if (node instanceof StringConstNode){
+        } else if (node instanceof StringConstNode) {
             node.setType(TreeConstants.Str);
-        }
-        else if (node instanceof StringConstNode){
+        } else if (node instanceof StringConstNode) {
             node.setType(TreeConstants.Str);
-        }
-        else if (node instanceof BoolConstNode){
+        } else if (node instanceof BoolConstNode) {
             node.setType(TreeConstants.Bool);
-        }
-        else{
-            //error for unknown class
+        } else {
+            // error for unknown class
         }
 
         return node.getType();
     }
 
     @Override
-    public Symbol visit(ObjectNode node, MyContext data){
-        //this needs to check the symbol table
-        node.setType(data.lookupVariable(node.getName()));
+    public Symbol visit(ObjectNode node, MySymbolTable table) {
+        // this needs to check the symbol table
+        TableData data = table.lookup(node.getName(), "variable");
+        if (data == null) {
+            Utilities.fatalError("cool error mate");
+        }
         return node.getType();
     }
 
     @Override
-    public Symbol visit(AssignNode node, MyContext data){
-        //this needs to add symbols to the symbol table
-        data.addId(node.getName(), new TableData("var", node.getType(), ""));
-        return node.getType();
+    public Symbol visit(AssignNode node, MySymbolTable table) {
+        // this needs to add symbols to the symbol table
+        table.addId(node.getName(), "variable", new TableData("properties of this thing"));
+        return node.getName();
     }
 }
