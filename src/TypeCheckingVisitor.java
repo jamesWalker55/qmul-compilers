ff943c4ed831ca43f8848bc57dfdb10a0d7e2d4e
Merv39 <78385901+Merv39@users.noreply.github.com>
2023-03-18 01:36:47 +0000

    Implement Inheritance graph

--- src/TypeCheckingVisitor.java
+++ src/TypeCheckingVisitor.java
@@ -9,8 +9,133 @@ import java.util.Stack;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+class MyInheritanceGraph{
+    //Symbol1 is the Object, Symbol2 is its parent
+    private Stack<HashMap<Symbol, Symbol>> tbl = new Stack<HashMap<Symbol, Symbol>>();
+
+    public MyInheritanceGraph(){
+        enterScope();
+        //Object is the root
+        addId(TreeConstants.Object_, null);
+        addId(TreeConstants.IO, TreeConstants.Object_);
+        addId(TreeConstants.Int, TreeConstants.Object_);
+        addId(TreeConstants.Str, TreeConstants.Object_);
+        addId(TreeConstants.Bool, TreeConstants.Object_);
+        System.out.println(this.toString());
+        System.out.println(conformance(TreeConstants.Int, TreeConstants.Object_));
+        System.out.println(conformance(TreeConstants.Object_, TreeConstants.Int));
+    }
+
+    // if A ≤ B
+    public boolean conformance(Symbol A, Symbol B){
+        if (A.getName().equals(B.getName())){
+            return true;
+        }
+        else if (lub(A,B).equals(B)){
+            //if A is a subclass, the lub is B
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    private ArrayList<Symbol> getPath(Symbol A){
+        ArrayList<Symbol> listA = new ArrayList<Symbol>();
+        while(A != null){
+            listA.add(0, A); //push to front of array list
+            A = lookup(A);
+        }
+        System.out.println(listA);
+        return listA;
+    }
+
+    //least upper bounds
+    public Symbol lub(Symbol A, Symbol B){
+        //go up to the top then keep going down until not the same
+        if (A.getName() == B.getName()){
+            return A;
+        }
+
+        ArrayList<Symbol> listA = getPath(A);
+        ArrayList<Symbol> listB = getPath(B);
+
+        int i = 0;
+        while (listA.get(i) == listB.get(i)){
+            if (i == listA.size()-1 || i == listB.size()-1){ break; }
+            i++;
+        }
+        System.out.println("same: "+ listA.get(i).getName());
+        return listA.get(i);
+    }
+
+    public void enterScope() {
+        tbl.push(new HashMap<Symbol, Symbol>());
+    }
+
+    /**
+     * Exits the most recently entered scope.
+     */
+    public void exitScope() {
+        if (tbl.empty()) {
+            Utilities.fatalError("existScope: can't remove scope from an empty symbol table.");
+        }
+        tbl.pop();
+    }
+
+    /**
+     * Adds a new entry to the symbol table.
+     *
+     * @param id   the symbol
+     * @param parent the data associated with id
+     */
+    public void addId(Symbol id, Symbol parent) {
+        if (tbl.empty()) {
+            Utilities.fatalError("addId: can't add a symbol without a scope.");
+        }
+        tbl.peek().put(id, parent);
+    }
+
+    /**
+     * Looks up an item through all scopes of the symbol table.  If
+     * found it returns the associated information field, if not it
+     * returns <code>null</code>.
+     *
+     * @param sym the symbol
+     * @return the parent of sym, or null if not found
+     */
+    public Symbol lookup(Symbol sym) {
+        if (tbl.empty()) {
+            Utilities.fatalError("lookup: no scope in symbol table.");
+        }
+        // I break the abstraction here a bit by knowing that stack is
+        // really a vector.
+        for (int i = tbl.size() - 1; i >= 0; i--) {
+            Symbol info = tbl.elementAt(i).get(sym);
+            if (info != null) return info;
+        }
+        return null;
+    }
+
+    /**
+     * Gets the string representation of the symbol table.
+     *
+     * @return the string rep
+     */
+    public String toString() {
+        String res = "";
+        // I break the abstraction here a bit by knowing that stack is
+        // really a vector...
+        for (int i = tbl.size() - 1, j = 0; i >= 0; i--, j++) {
+            res += "Scope " + j + ": " + tbl.elementAt(i) + "\n";
+        }
+        return res;
+    }
+}
+
 class MySymbolTable {
     private Stack<HashMap<List<String>, TableData>> tbl = new Stack<HashMap<List<String>, TableData>>();
+    public MyInheritanceGraph graph = new MyInheritanceGraph();
 
     public MySymbolTable() {
         enterScope();
@@ -251,20 +376,22 @@ public class TypeCheckingVisitor extends BaseVisitor<Symbol, MySymbolTable> {
         return node.getType();
     }
 
-    // @Override
-    // public Symbol visit(AssignNode node, MySymbolTable table) {
-    //     TableData data = table.lookup(node.getName(), "variable");
-    //     //O(Id) = T
-    //     if (!visit(node.getE1(), data).equals(TreeConstants.Int)) {
-    //     Symbol T = data.getType();
-    //     //if type of e1 is not equal to T'
-    //     if (!E.equals(T2))
+    @Override
+    public Symbol visit(AssignNode node, MySymbolTable table) {
+        TableData data = table.lookup(node.getName(), "variable");
+        //O(Id) = T
+        Symbol T = data.getType();
 
-    //     //O, M, C |- e1 : T'
-    //     Symbol T2 = visit((ExpressionNode)node.getExpr(), table);
+        //if type of e1 is not equal to T'
+        //O, M, C |- e1 : T'
+        if (visit(node.getExpr(), table).equals(T)){
+            //error
+        }
 
-    //     // this needs to add symbols to the symbol table
-    //     table.addId(node.getName(), "variable", new TableData(node.getName()));
-    //     return node.getName();
-    // }
+        Symbol T2 = visit((ExpressionNode)node.getExpr(), table);
+
+        // this needs to add symbols to the symbol table
+        table.addId(node.getName(), "variable", new TableData(node.getName()));
+        return node.getName();
+    }
 }
