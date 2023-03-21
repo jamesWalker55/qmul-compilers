9ae528f1ceff26ae555a192bc012b8716fa58ae9
James Walker <james.chunho@gmail.com>
2023-03-21 14:24:47 +0000

    script for easy testing of semant

---
+++ run-pa2.sh
@@ -0,0 +1,19 @@
+if [ -z "$1" ]
+then
+    echo "Please provide a path to a cool file."
+else
+    # determine actual path of input file
+    input_script=$(readlink -m "$1")
+    # # build the frontend first
+    # echo "Building frontend..."
+    # cd $COOL_HOME/assignments/pa1
+    # buildme frontend
+    # then build the semant
+    echo "Building semant..."
+    cd $COOL_HOME/assignments/pa2
+    buildme semant
+    # finally run the semant
+    echo "Running mysemant..."
+    # cd $last_working_dir
+    ./mysemant "$input_script"
+fi
