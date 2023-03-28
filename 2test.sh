1b4f4a4d37889341df63f8078e9e2135f84fd5cb
James Walker <james.chunho@gmail.com>
2023-03-28 01:58:13 +0100

    better test script

--- 2test.sh
+++ 2test.sh
@@ -1,2 +1,10 @@
+# create folder for test output
+mkdir -p $COOL_HOME/testoutput
+
+# output path
+DATE=$(date '+%Y-%m-%d_%H-%M-%S')
+OUTPUT_PATH="$COOL_HOME/testoutput/$DATE.txt"
+
 cd $COOL_HOME/assignments/pa2
-testme semant
+date '+%Y-%m-%d %H:%M:%S'
+testme semant 2>&1 | tee outfile $OUTPUT_PATH
