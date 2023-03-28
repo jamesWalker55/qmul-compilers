bb360d1234e0930c872fcd69d8efefe90c6fecc9
James Walker <james.chunho@gmail.com>
2023-03-28 14:06:39 +0100

    Fix 2test script

--- 2test.sh
+++ 2test.sh
@@ -7,4 +7,4 @@ OUTPUT_PATH="$COOL_HOME/testoutput/$DATE.txt"
 
 cd $COOL_HOME/assignments/pa2
 date '+%Y-%m-%d %H:%M:%S'
-testme semant 2>&1 | tee outfile $OUTPUT_PATH
+testme semant 2>&1 | tee $OUTPUT_PATH
