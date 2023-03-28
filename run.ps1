63c7cf81a348d53b1c030f855ae8b43e22cbf5d1
James Walker <james.chunho@gmail.com>
2023-03-28 02:03:51 +0100

    delete old unused files

--- run.ps1
+++
@@ -1,9 +0,0 @@
-# example usage:
-# .\run.ps1 assignments\pa1\good.cl
-
-$ANTLR_PATH = "."
-$BUILD_DIR = "$ANTLR_PATH\assignments\pa1"
-$ANTLR_JAR = "$ANTLR_PATH\lib\antlr-4.6-complete.jar"
-$CLASSPATH = "$BUILD_DIR;$ANTLR_JAR;"
-
-java -classpath $CLASSPATH Frontend $args
